13b95b57014f2bd0f62cff0f4d9a77e4de6b886e
Phil Tietjen <tietjen.philip@gmail.com>
2019-08-26 11:30:45 -0400

    Add Button Component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text} from '../Text'
+import styled from '@emotion/styled'
+
+/* globals tw */
+
+const Button = ({children, ...attrs}) => {
+  return(
+    <StyledButton {...attrs}>{children}</StyledButton>
+  )
+}
+
+const StyledButton = styled.button`${tw`font-sans no-underline border-none rounded bg-blue text-white py-2 px-4`}`
+
+export default Button
